26243b4369f0e703fcf875a69bcdfc872d7a8feb
Catalina Rosales <131308438+rataxikita@users.noreply.github.com>
2025-05-15 23:14:59 -0400

    Init Repo

---
+++ src/main/java/com/edutech/edutech_api/EdutechApiApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.edutech_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutechApiApplication.class, args);
+	}
+
+}
