2ab5d0820b9741837a2d52c8796d098dbc43e2e0
DiegoInacioGit <d.sotelo@duocuc.cl>
2025-05-26 21:36:20 -0400

    incidencia v2

--- src/main/java/com/duoc/Edutech/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.duoc.Edutech.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.duoc.Edutech.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
